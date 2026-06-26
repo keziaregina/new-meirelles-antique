be7349a393ed697077a10cfe88cd9609b7fcbf90
Kezia Regina <keziaregina28@gmail.com>
2026-06-26 15:50:47 +0700

    update content in product page

--- catalog/language/en-gb/product/product.php
+++ catalog/language/en-gb/product/product.php
@@ -3,12 +3,12 @@
 $_['text_search']              = 'Search';
 $_['text_brand']               = 'Brand';
 $_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
+$_['text_model']               = 'Stock ID:';
 $_['text_reward']              = 'Reward Points:';
 $_['text_points']              = 'Price in reward points:';
 $_['text_stock']               = 'Availability:';
 $_['text_instock']             = 'In Stock';
-$_['text_tax']                 = 'Ex Tax:';
+$_['text_tax']                 = 'Incl. Tax:';
 $_['text_discount']            = ' or more ';
 $_['text_option']              = 'Available Options';
 $_['text_minimum']             = 'This product has a minimum quantity of %s';
