39f0ff80e204a549366cb8e3dd355880ead5e81e
Kezia Regina <keziaregina28@gmail.com>
2026-07-11 10:27:52 +0700

    update teks in table cart page

--- catalog/language/en-gb/checkout/cart.php
+++ catalog/language/en-gb/checkout/cart.php
@@ -29,7 +29,7 @@ $_['cart_heading']             = 'Checkout';
 // Column
 $_['column_image']             = 'Image';
 $_['column_name']              = 'Product Name';
-$_['column_model']             = 'Model';
+$_['column_model']             = 'Stock ID';
 $_['column_quantity']          = 'Quantity';
 $_['column_price']             = 'Unit Price';
 $_['column_total']             = 'Total';
